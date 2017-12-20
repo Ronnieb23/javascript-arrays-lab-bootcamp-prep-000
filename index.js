8b17605be136f52aa8c389d724e67f4aa8dcd690
Ronald Bennett <bennettrd318@gmail.com>
2017-12-20 23:56:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+ var kitOnEnd = kittens.push(name);
+ return kitOnEnd;
+}
+
+function destructivelyPrependKitten(name) {
+  var kitOnFront = kittens.unshift(name);
+  return kitOnFront;
+}
+
+function destructivelyRemoveLastKitten() {
+  var removeLastKitten = kittens.pop();
+  return removeLastKitten;
+}
+
+function destructivelyRemoveFirstKitten() {
+  var removeFirstKitten = kittens.shift();
+  return removeFirstKitten;
+}
+
+function appendKitten(name) {
+  appendKitten(name);
+  return kittens;
+}
+
+function prependKitten(name) {
+  var prepKit = prependKittens(name);
+  return kittens;
+}
